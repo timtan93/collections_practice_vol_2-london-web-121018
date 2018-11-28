2688e5141dcd03a9aa40c95cb8413fd44f00632e
timtan93 <timtan93@gmail.com>
2018-11-28 22:59:34 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
 # your code goes here
+def begins_with_r(array)
+  i = 0
+  while i < array.length
+    if array[i][0] != "r"
+      return false
+    end
+    i += 1
+  end
+  true
+end
+
+def contain_a(array)
+  i = 0
+  contains_a = []
+  while i < array.length
+    if array[i].include? "a"
+      contains_a << array[i]
+    end
+    i += 1
+  end
+  contains_a
+end
+
+def first_wa(array)
+  i = 0
+  while i < array.length
+    if array[i][0] != "w" && array[i][1] != "a"
+      return false
+    end
+    i += 1
+  end
+  true
+end
