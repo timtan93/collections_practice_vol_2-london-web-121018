9d42fd0169dc518f0a05f5ba6b4629ee399e44e9
Timothy Tan <timtan93@gmail.com>
2018-12-03 20:28:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
 # your code goes here
+def begins_with_r(array)
+  r = true
+  array.each do |element|
+    r = false if element[0] != "r"
+  end
+  r
+end
+
+def contain_a(array)
+  a_array = []
+  array.each do |element|
+     if element.include?("a")
+       a_array << element
+  end
+  a_array
+end
+end
